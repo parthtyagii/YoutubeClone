2b5c0526ab02e2b8752ac81794d6bad9162d6998
Parth Tyagi <parthtyagicode@gmail.com>
2023-05-30 00:41:25 +0530

    added responsiveness

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -8,21 +8,20 @@ function Modal({ data, setPlayVideo }) {
 
     return (
         <div className='videoPlayerContainer'>
-            <div className="extra">
-                <div className="videoPlayer">
+            <div className="videoPlayer">
+                <button className='close'>
+                    <VscClose onClick={() => setPlayVideo(false)} />
+                </button>
 
-                    <button className='close'>
-                        <VscClose onClick={() => setPlayVideo(false)} />
-                    </button>
+                <div className="video">
+                    <video controls autoPlay loop >
+                        <source src={data.submission.mediaUrl} />
+                    </video>
+                </div>
 
-                    <div className="video">
-                        <video controls autoPlay loop muted width='200px'>
-                            <source src={data.submission.mediaUrl} />
-                        </video>
-                    </div>
-
+                <div className="videoInfo">
                     <div className="videoTitle">{data.submission.title}</div>
-
+                    <div className="videoCreator">Created by {data.creator.name}</div>
                     <div className="videoDescription">{data.submission.description}</div>
                 </div>
             </div>
